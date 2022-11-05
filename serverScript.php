c256dc048859d2b151c108e1bbf7d6887cb1f11b
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-11-05 14:35:38 +0800

    dXML

--- serverScript.php
+++ serverScript.php
@@ -1,28 +1,45 @@
 <?php
-$q=$_GET["q"];
-
-$xmlDoc = new DOMDocument();
+$xmlDoc=new DOMDocument();
 $xmlDoc->load("dynamicXML.xml");
 
 $x=$xmlDoc->getElementsByTagName('link');
 
-for ($i=0; $i<=$x->length-1; $i++) {
-  //Process only element nodes
-  if ($x->item($i)->nodeType==1) {
-    if ($x->item($i)->childNodes->item(0)->nodeValue == $q) {
-      $y=($x->item($i)->parentNode);
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('company');
+    $z=$x->item($i)->getElementsByTagName('product');
+    if ($y->item(0)->nodeType==1) {
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint="<a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        } else {
+          $hint=$hint . "<br /><a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        }
+      }
     }
   }
 }
 
-$cd=($y->childNodes);
-
-for ($i=0;$i<$cd->length;$i++) {
-  //Process only element nodes
-  if ($cd->item($i)->nodeType==1) {
-    echo("<b>" . $cd->item($i)->nodeName . ":</b> ");
-    echo($cd->item($i)->childNodes->item(0)->nodeValue);
-    echo("<br>");
-  }
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response=$hint;
 }
+
+//output the response
+echo $response;
 ?>
