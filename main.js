9a1963e7dcdacecf5ab60620e2577cf60c1fb388
Wilzen Carl R. Tolentino <109589426+OnezPH@users.noreply.github.com>
2022-11-05 12:31:00 +0800

    dXML

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function showResult(str) {
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
-      document.getElementById(item).innerHTML = this.responseText;
     }
   }
   xmlhttp.open("GET","serverScript.php?q="+str,true);
