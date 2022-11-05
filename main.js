dbb78fc01356081c0a3a33b334333ac2f1fd8518
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-11-05 10:54:52 +0800

    dXML

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ function showResult(str) {
       document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     }
   }
-  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.open("GET","server.php?q="+str,true);
   xmlhttp.send();
 }
