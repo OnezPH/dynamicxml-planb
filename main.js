73a747036a1128adc56d03d8c6de9603a097f707
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-11-05 11:28:32 +0800

    dXML

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 function showResult(str) {
-  if (str.length==0) {
-    document.getElementById("livesearch").innerHTML="";
-    document.getElementById("livesearch").style.border="0px";
-    return;
-  }
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("livesearch").innerHTML=this.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+  if (str.length == 0) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      myFunction(this);
     }
+    xhttp.open("GET", "dynamicXML.xml");
+    xhttp.send();
   }
-  xmlhttp.open("GET","server.php?q="+str,true);
-  xmlhttp.send();
 }
+function myFunction(xml) {
+  const xmlDoc = xml.responseXML;
+  const x = xmlDoc.getElementsByTagName("link");
+  let table = "";
+  for (let i = 0; i < x.length; i++) { 
+    table += "<div class='item_wrap'> <h1>" +
+    x[i].getElementsByTagName("company")[0].childNodes[0].nodeValue +
+    "</h1><br>" +
+    x[i].getElementsByTagName("product")[0].childNodes[0].nodeValue +
+    "</div>";
+  }
+  document.getElementById("item").innerHTML = table;
+}
+console.log(showResult);
+console.log(myFunction);
