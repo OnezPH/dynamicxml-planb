fe0b474e7aaaa8699eac1b8fda803810c78908cd
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-11-05 12:18:27 +0800

    dXML

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function showResult(str) {
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
-      document.getElementById(item).display = "contents";
+      document.getElementById(item).display = "block";
       document.getElementById("item").innerHTML=this.responseText;
     }
   }
